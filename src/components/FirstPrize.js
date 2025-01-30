a53f2e2d8f06cf0b37b40064d1d79e93fb4ea9b5
Anuj Chopra <anujchopra100@gmail.com>
2025-01-30 15:32:59 +0530

    first commit

---
+++ src/components/FirstPrize.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "./Spinner";
+import SliderContainer from "./SliderContainer";
+import Modal from "./Modal";
+import { motion } from "framer-motion";
+
+function FirstPrize() {
+  const [isCardVisible, setIsCardVisible] = useState(true);
+  const [isSpinnerVisible, setIsSpinnerVisible] = useState(false);
+
+  const [revealPrize, setRevealPrize] = useState(false);
+  const [showWinners, setShowWinners] = useState(false);
+  const [isShowing, setIsShowing] = useState(true);
+  const [announceWinner, setAnnounceWinner] = useState(false);
+
+  const toggleElement = () => {
+    setIsShowing(true);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    setAnnounceWinner(!announceWinner);
+  };
+
+  const handleCardClick = () => {
+    setIsCardVisible(false);
+    setIsSpinnerVisible(true);
+  };
+
+  const winnersList = [
+    { url: "/coromandel_winner_posters/first_prize.gif", title: "beach" },
+  ];
+
+  const handleAnnounceImage = () => {
+    setShowWinners(true);
+    setIsShowing(true);
+    setAnnounceWinner(false);
+
+    setShowModal(!showModal);
+  };
+
+  return (
+    <div className="grandpage">
+      <div className="nav-buttons-container">
+        <Link to={"/"}>
+          <motion.button
+            whileHover={{ scale: "1.2", color: "yellow" }}
+            whileTap={{ scale: "0.9" }}
+            className="home-button medium-home-button"
+          >
+            Home
+          </motion.button>
+        </Link>
+      </div>
+      <div className="grandPrize">
+        <img
+          src="/requirements/backgrounds/first-winner-up.gif"
+          className="grandprize-image"
+          alt=""
+        />
+      </div>
+      <div className="grandprize-down-portion">
+        <img
+          className="grand-down-image"
+          src="/requirements/backgrounds/first-winner-down.gif"
+          alt=""
+        />
+        <div className="items-container">
+          {isCardVisible && (
+            <motion.div
+              initial={{ y: "-100vh" }}
+              animate={{ y: "0" }}
+              transition={{ delay: "0.6" }}
+              className="grand-page-card-container"
+            >
+              <div className="card" onClick={handleCardClick}>
+                <div className="card-image-container">
+                  <img src="2.gif" className="card-image" alt="" />
+                </div>
+              </div>
+            </motion.div>
+          )}
+          {isSpinnerVisible && (
+            <Spinner
+              isSpinnerVisible={isSpinnerVisible}
+              setIsSpinnerVisible={setIsSpinnerVisible}
+              announceWinner={announceWinner}
+              setAnnounceWinner={setAnnounceWinner}
+              setRevealPrize={setRevealPrize}
+              setIsShowing={setIsShowing}
+            />
+          )}
+          {announceWinner && (
+            <motion.div
+              // initial={{rotate:"180"}}
+              // animate={{rotate:"0"}}
+              
+             
+              className={`transition-element ${isShowing ? "show" : ""}`}
+              onClick={handleAnnounceImage}
+            >
+              <img
+                src="/images/third_step_image.gif"
+                className="announce-prize-image"
+                alt="grand-prize-announcement"
+              />
+            </motion.div>
+          )}
+
+          {/* {
+            showWinners && (
+                <div className={`transition-element ${isShowing ? 'show' : ''}`} >
+                     <img src="grandprizeWinners.gif" className="grand-winners-image" alt="grand-prize-announcement" />
+                </div>
+            )
+
+        } */}
+          {/* {
+            showWinners && (<SliderContainer winnersList={winnersList} />)
+        } */}
+        </div>
+      </div>
+      {showWinners && (
+        <Modal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          toggleModal={toggleModal}
+          winnersList={winnersList}
+          revealPrize={revealPrize}
+          setRevealPrize={setRevealPrize}
+        />
+      )}
+    </div>
+  );
+}
+
+export default FirstPrize;
---
+++ src/components/Home.js
@@ -0,0 +1,137 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import CarouselC from "./CarouselC";
+import "../carousal.css";
+import { ShowNewGrandPageContext } from "./GrandPageContext";
+import { motion } from "framer-motion";
+
+function Home() {
+  const [
+    showSecondGrandPage,
+    setShowSecondGrandPage,
+    showGrandPage2,
+    setShowgrandPage2,
+  ] = useContext(ShowNewGrandPageContext);
+
+  // if(showSecondGrandPage===false){
+  //   setShowSecondGrandPage(true)
+  // }
+
+  // if(showGrandPage2===false){
+  //   setShowgrandPage2(true)
+  // }
+
+  if (localStorage.getItem("setShowSecondGrandPage") === true) {
+  }
+
+  return (
+    <>
+      <div className="outer"
+
+       >
+        <div className="home">
+          <div className="nav-buttons-container">
+            <Link to={"/"}>
+<<<<<<< HEAD
+              <button className="home-button medium-home-button">Home.</button>
+=======
+              <button className="home-button medium-home-button">Home</button>
+>>>>>>> 8830cddaf258d510281477fe4deb22f69430be80
+            </Link>
+            {/* <Link to={"/"} >
+        <button className="home-button medium-home-button" >Reset</button>
+        </Link> */}
+          </div>
+          <img
+            src="/requirements/backgrounds/home-up.gif"
+            className="banner-image"
+            alt=""
+          />
+        </div>
+        {/* <div className="home-down-portion"> */}
+
+        <div className="card-container">
+          <img
+            className="home-down-image"
+            src="/requirements/backgrounds/home-down.gif"
+            alt=""
+          />
+          <Link
+            to={
+              showSecondGrandPage || showGrandPage2
+                ? "/secondgrandprize"
+                : "/grandprize"
+            }
+            className="button-link"
+          >
+            <div className="card">
+              <div className="card-image-container">
+                <img src="1.gif" className="card-image" alt="" />
+              </div>
+              {/* <Link to="/grandprize" className="button-link"><button class="button">
+            <span>Grand Prize</span>
+          </button></Link>  */}
+            </div>
+          </Link>
+          <Link to="/firstprize" className="button-link">
+            <div className="card">
+              <div className="card-image-container">
+                <img src="2.gif" className="card-image" alt="" />
+              </div>
+              {/* <Link to="/1stPrize" className="button-link"><button class="button">
+            <span>1st Prize</span>
+          </button></Link> */}
+            </div>
+          </Link>
+          <Link to="/secondprize" className="button-link">
+            <div className="card">
+              <div className="card-image-container">
+                <img src="3.gif" className="card-image" alt="" />
+              </div>
+
+              {/* <Link to="/2ndPrize" className="button-link"><button class="button">
+            <span>2nd Prize</span>
+          </button></Link> */}
+            </div>
+          </Link>
+          <Link to="/thirdprize" className="button-link">
+            <div className="card">
+              <div className="card-image-container">
+                <img src="4.gif" className=" card-image" alt="" />
+              </div>
+              {/* <Link to="/3rdPrize" className="button-link"><button class="button">
+            <span>3rd Prize</span>
+          </button></Link> */}
+            </div>
+          </Link>
+          {/* <div className="inner-container"> */}
+          <Link to="/fourthprize" className="button-link">
+            <div className="card">
+              <div className="card-image-container">
+                <img src="5.gif" className="card-image" alt="" />
+              </div>
+              {/* <Link to="/4thPrize" className="button-link"><button class="button">
+              <span>4rth Prize</span>
+            </button></Link> */}
+            </div>
+          </Link>
+          <Link to="/fifthprize" className="button-link">
+            <div className="card">
+              <div className="card-image-container">
+                <img src="6.gif" className=" card-image" alt="" />
+              </div>
+              {/* <Link to="/5thPrize" className="button-link"><button class="button">
+              <span>5th Prize</span>
+            </button></Link> */}
+            </div>
+          </Link>
+          {/* </div> */}
+        </div>
+
+        {/* </div> */}
+      </div>
+    </>
+  );
+}
+
+export default Home;
